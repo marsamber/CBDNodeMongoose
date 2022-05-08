149fe7d1f17cc72e7cead4b0aef27fdb7a284e42
Marta Sampedro Bernal <sbmarta2000@gmail.com>
2022-05-08 15:10:14 +0200

    Change order in toCook and Cooked list

--- frontend/src/modules/recipes/Cooked.js
+++ frontend/src/modules/recipes/Cooked.js
@@ -8,26 +8,26 @@ import cookedAPI from "../APIs/cookedAPI";
 const Cooked = () => {
     if (!authenticated.isLogged()) window.location.href = '/';
 
-    const [recipes, setRecipes] = useState([]);
+    // const [recipes, setRecipes] = useState([]);
     const [items, setItems] = useState([]);
 
     useEffect(() => {
         cookedAPI.getAllCooked().then((cooked) => {
             setItems(cooked);
-            var recipes = [];
-            for (let i = 0; i < cooked.length; i++) {
-                const c = cooked[i];
-                recipes.push(c.recipe)
-            }
-            setRecipes(recipes);
+            // var recipes = [];
+            // for (let i = 0; i < cooked.length; i++) {
+            //     const c = cooked[i];
+            //     recipes.push(c.recipe)
+            // }
+            // setRecipes(recipes);
         }).catch((err) => console.log(err));
     }, [])
 
-    if(!recipes) return <>Loading...</>
+    if(!items) return <>Loading...</>
     return <><MenuProfile />
         <br/>
         <h1 className="title text-center">Cooked</h1>
-        <RecipesList recipes={recipes} items={items} val="Like" /></>
+        <RecipesList items={items} val="Like" /></>
 }
 
 export default Cooked;
--- frontend/src/modules/recipes/ToCook.js
+++ frontend/src/modules/recipes/ToCook.js
@@ -8,26 +8,48 @@ import toCookAPI from "../APIs/toCookAPI";
 const ToCook = () => {
     if (!authenticated.isLogged()) window.location.href = '/';
 
-    const [recipes, setRecipes] = useState([]);
+    // const [recipes, setRecipes] = useState([]);
     const [items, setItems] = useState([]);
 
     useEffect(() => {
         toCookAPI.getAllToCook().then((toCook) => {
             setItems(toCook);
-            var recipes=[];
-            for (let i = 0; i < toCook.length; i++) {
-                const tc = toCook[i];
-                recipes.push(tc.recipe)
-            }
-            setRecipes(recipes);
+            // var recipes=[];
+            // for (let i = 0; i < toCook.length; i++) {
+            //     const tc = toCook[i];
+            //     recipes.push(tc.recipe)
+            // }
+            // setRecipes(recipes);
         }).catch((err) => console.log(err));
     }, [])
 
-    if(!recipes) return <>Loading...</>
+    const sortByPriority = (a,b) => {
+        if (a.priority === "HIGH") {
+            if (b.priority === "HIGH") {
+                return (a.recipe.title > b.recipe.title) ? 1 : ((b.recipe.title > a.recipe.title) ? -1 : 0);
+            } else {
+                return -1;
+            }
+        } else if (b.priority === "HIGH") {
+            return 1;
+        } else if (a.priority === "LOW") {
+            if (b.priority === "LOW") {
+                return (a.recipe.title > b.recipe.title) ? 1 : ((b.recipe.title > a.recipe.title) ? -1 : 0);
+            } else {
+                return 1;
+            }
+        } else if (b.priority === "LOW") {
+            return -1;
+        } else {
+            return (a.recipe.title > b.recipe.title) ? 1 : ((b.recipe.title > a.recipe.title) ? -1 : 0);
+        }
+    }
+
+    if(!items) return <>Loading...</>
     return <><MenuProfile />
         <br/>
         <h1 className="title text-center">To Cook</h1>
-        <RecipesList recipes={recipes} items={items} val="Priority" /></>
+        <RecipesList items={items} sortByPriority={sortByPriority} val="Priority" /></>
 }
 
 export default ToCook;
